3df78c83668d03b24c97fb95972780c1828a8102
EVELYN LAVIGNE GOMES DE LIMA <21193113@CFP127ALUNO.LOCAL>
2021-09-29 11:16:06 -0300

    melhorias adicionadas

--- galeria.js
+++ galeria.js
@@ -13,12 +13,13 @@ const imagens = [
 
 //MAP - FILTER - REDUCE -> Declarativa
 
+const limparId = (url) => url.replace("./img/","").split(".")[0].replace("", "-")
 
 const criarItem = (urlImagem) => {
     const container = document.querySelector(".galeria-container")
     
     const novoLink = document.createElement("a")
-    novoLink.href = urlImagem
+    novoLink.href = `#${limparId(urlImagem)}`
     novoLink.classList.add("galeria-itens")
     novoLink.innerHTML = `<img src="${urlImagem}" alt=""></img`
     container.appendChild(novoLink)
@@ -31,9 +32,36 @@ const criarItem = (urlImagem) => {
     // `
 }
 
-const carregarImagens = () => imagens.forEach(criarItem) 
+const carregarGaleria = () => imagens.forEach(criarItem) 
 
-carregarImagens()
+const criarSlide = (urlImagem, indice, arr) => {
+    const container = document.querySelector(".slide-container")
+    const novoDiv = document.createElement("div")
+    novoDiv.classList.add("slide")
+    novoDiv.id = limparId(urlImagem)
+
+    const indiceAnterior = indice <= 0 ? arr.length -1 : indice - 1
+    const idAnterior = limparId(arr[indiceAnterior])
+
+    const indiceProximo = indice >= arr.length -1 ? 0 : indice + 1
+    const idProximo = limparId(arr[indiceProximo])
+
+    novoDiv.innerHTML = `
+                <div class="image1">
+                    <a href="" class="icones fechar">&#10006;</a>
+                    <a href="#${idAnterior}" class="icones anterior">&#171;</a>
+                    <img src="${urlImagem}" alt="">
+                    <a href="#${idProximo}" class="icones proximo">&#187;</a>
+                </div>
+    `
+    container.appendChild(novoDiv)
+}
+
+const carregarSlide = (imagens) => imagens.forEach(criarSlide)
+
+carregarGaleria(imagens)
+carregarSlide(imagens)
+
 
 
 
